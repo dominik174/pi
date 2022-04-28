657349805cc0445cf8a10a83fed0b0f86e2ba5e1
Dominik <dominik.zlepalo@gmail.com>
2022-04-28 13:13:06 +0200

    ok

--- DominikTest/t.2.py
+++ DominikTest/t.2.py
@@ -1,17 +1,50 @@
 import keyboard
+import random
 from gpiozero import LED
 from time import sleep
 
-a = LED(25)
-b = LED(17)
-c = LED(13)
-d = LED(20)
+# dictionary
+led_pins = {
+    1:25,
+    2:17,
+    3:13,
+    4:20
+}
+
+# Pali led
+def pali_led(x):
+    led_nr = led_pins[x]
+    a = LED(led_nr)
+    a.on()
+    sleep(1)
+    a.off()
 
 numbers = []
 
-for x in "sasa":
-    n = random.randint(1,4)
-    numbers.append(n)
+# generiranje radnom brojeva
+def random_br():
+    for x in "sasa":
+        n = random.randint(1,4)
+        numbers.append(n)
+    for x in numbers:
+        pali_led(x)
 
-for x in numbers:
-    
+random_br()
+print(numbers)
+# string - array
+print("Upisite kako su se palile lampice")
+brojevi = input()
+brojevi_arr = []
+for x in brojevi:
+    brojevi_arr.append(int(x))
+
+while numbers == brojevi_arr:
+    random_br()
+    print("Upisite kako su se palile lampice")
+    brojevi = input()
+    brojevi_arr = []
+    for x in brojevi:
+        brojevi_arr.append(int(x))
+
+if numbers != brojevi_arr:
+    print("Krivo ste brojeve upisali! Game over!")
