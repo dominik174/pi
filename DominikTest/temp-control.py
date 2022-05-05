95eb35a579f12fc17bc54e544db1c7292541696c
Dominik <dominik.zlepalo@gmail.com>
2022-05-05 10:38:10 +0200

    klmkmklmlkmlkm cffgf

--- DominikTest/temp-control.py
+++ DominikTest/temp-control.py
@@ -1,10 +1,12 @@
 import RPi.GPIO as GPIO
 import time
 import os
+from datetime import datetime
+import pandas as pd
 
 MotorPin_A  = 11
 MotorPin_B  = 12
-
+temperature = 0
 # def motorStop():
 # 	GPIO.output(MotorPin_A, GPIO.HIGH)
 # 	GPIO.output(MotorPin_B, GPIO.HIGH)
@@ -28,14 +30,14 @@ def motor(status, direction):
 		GPIO.output(MotorPin_A, GPIO.HIGH)
 		GPIO.output(MotorPin_B, GPIO.HIGH)
 
-def loop1():
-	while True:
-		motor(1, 1)
-		time.sleep(5000)
-		motor(0, 1)
-		time.sleep(5000)
-		motor(1, 0)
-		time.sleep(5000)
+# def loop1():
+# 	while True:
+# 		motor(1, 1)
+# 		time.sleep(5000)
+# 		motor(0, 1)
+# 		time.sleep(5000)
+# 		motor(1, 0)
+# 		time.sleep(5000)
 
 def destroyMotor():
 	motor(0,0)
@@ -48,9 +50,13 @@ def readSensor(id):
 	secondline = text.split("\n")[1]
 	temperaturedata = secondline.split(" ")[9]
 	temperature = float(temperaturedata[2:])
-	global temperature
 	temperature = temperature / 1000
+	now = datetime.now()
+	current_time = now.strftime("%H:%M:%S")
 	print("Sensor: " + id  + " - Current temperature : %0.3f C" % temperature)
+	f = open('TempMessen.txt', 'a')
+	f.write(current_time + " - Current temperature: " + str(temperature) + "C\n")
+	f.close()
 
 # Reads temperature from all sensors found in /sys/bus/w1/devices/
 # starting with "28-...
@@ -97,4 +103,11 @@ if __name__ == "__main__":
 		start()
 	except KeyboardInterrupt:
 		totalStop()
-		
+		file = open('TempMessen.txt', 'r')
+		data = [file.readlines()]
+		file.close
+		print(data)
+		infile = r'/home/pi/Documents/VSSCode/pi/DominikTest/TempMessen.txt'
+# Use skiprows to choose starting point and nrows to choose number of rows
+		numbers = pd.read_csv(infile, skiprows = 32, nrows=6)
+		print(sum(numbers) / len(numbers))
